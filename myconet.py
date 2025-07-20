4b0943e4fda08560a226f7e8275c7a2617f83706
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2025-07-20 14:54:45 +0000

    First Deployment

--- myconet.py
+++ myconet.py
@@ -1,1 +1,29 @@
+import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+import tempfile
+from tensorflow.keras.models import load_model
 
+@st.cache_resource
+def loading_model():
+    try:
+        model = load_model('./model/model4.h5')
+        return model
+    except Exception as e:
+        print (f'Fail to Load Model. Error: {e}')
+
+model = loading_model()
+
+st.set_page_config(
+    page_title="Microscopic Fungus Colony Classifier",
+    page_icon="🧫",
+    layout="wide"
+)
+
+# ========== TITLE ==========
+st.markdown(
+    "<h1 style='text-align: center; color: #4B8BBE;'>Microscopic Fungus Colony Classification with CNN Resedual Network </h1>",
+    unsafe_allow_html=True
+)
+st.markdown("<hr>", unsafe_allow_html=True)
