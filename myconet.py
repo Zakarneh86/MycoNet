c39f86325544af65a3cf3ec0bbedb89cd5ac2ee0
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2025-07-25 10:26:25 +0000

    6.1

--- myconet.py
+++ myconet.py
@@ -103,7 +103,7 @@ if uploaded_file:
     col1, col2 = st.columns(2)
     with col1:
         st.image(Image.fromarray(original_img_rgb), caption="📷 Original Image")
-        st.write("Predictions:", results.numpy())
+        #st.write("Predictions:", results.numpy())
         st.write("Loss:", results[:, class_ids])
     with col2:
         layer = st.selectbox("Choose a layer:", ['conv2d', 'conv2d_1', 'conv2d_2', 'conv2d_3', 'conv2d_4', 'conv2d_5', 'conv2d_8', 'conv2d_9', 'conv2d_7'])
