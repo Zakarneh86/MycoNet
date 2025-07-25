c8213ec716e2ba3030e066bf6e942baa5707dfe9
Zakarneh86 <98852242+Zakarneh86@users.noreply.github.com>
2025-07-25 09:53:35 +0000

    4

--- myconet.py
+++ myconet.py
@@ -98,13 +98,13 @@ if uploaded_file:
     st.sidebar.success(f"**Confidence:** {conf:.2f}")
 
     heatmaps = grad_cam(model, original_img_rgb)
-    grad_cam_img = heatmaps['conv2d_7']
+    grad_cam_img = heatmaps['conv2d_9']
 
     if grad_cam_img is not None:
         # Convert to uint8 and apply colormap
         heatmap_uint8 = np.uint8(255 * grad_cam_img)
         heatmap_colored = cv2.applyColorMap(heatmap_uint8, cv2.COLORMAP_JET)
-        #heatmap_colored = cv2.cvtColor(heatmap_colored, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB for PIL
+        heatmap_colored = cv2.cvtColor(heatmap_colored, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB for PIL
 
         grad_cam_display = Image.fromarray(heatmap_colored)
     else:
